07d9aea57e0a4d8dad77b4518bf602025898a458
Kevin Lam <Kevin@Administrators-MacBook-Pro.local>
2023-05-04 13:10:07 -0400

    first

---
+++ Hard/script.js
@@ -0,0 +1,18 @@
+// HARD: Data security exercise. Inside of a closure, create an object called pii (Personally Identifiable Information)that cannot be accessed directly. The object should have at least two properties: name and ssn. Only the name property should be accessible, and it should be called through a public function. The ssn property should not be accessible at all.
+// Creating private objects and private properties helps you control who has access to what data and helps you prevent people who shouldn't see important info like social security numbers from getting access to the data.
+// You can use 'getName' or other get methods to access data that people might need. For example, people addressing a package or email may need a customer's name, but they definitely shouldn't have access to their ssn.
+
+function person(name, ssn) {
+  var pii = {
+    name: name,
+    ssn: ssn,
+  };
+  return {
+    getName: function () {
+      return pii.name;
+    },
+  };
+}
+var person = person("Osiel", "123-45-6789");
+console.log(person.getName());
+console.log(person.ssn);
